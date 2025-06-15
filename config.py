b27bb457d9cf439e0dc6d1736411130a014b2218
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-15 21:06:42 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "10389378"))
     API_HASH = os.environ.get("API_HASH", "cdd5c820cb6abeecaef38e2bb8db4860")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7513275210:AAGgRzfVk1_nEaekouSY1kWmp0eqD3pNw-I")
-    ADMIN_ID = int(os.environ.get("ADMIN_ID", "7944526906"))
+    ADMIN_ID = int(os.environ.get("ADMIN_ID", "1938030055"))
 
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "telegram_bot")
