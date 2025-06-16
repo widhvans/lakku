a12b78dbbd0fd9b531ee18c45a8b2d2c8d59e5d3
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-16 16:14:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,17 +1,26 @@
 import os
 
 class Config:
+    # Your API details from my.telegram.org
     API_ID = int(os.environ.get("API_ID", "10389378"))
     API_HASH = os.environ.get("API_HASH", "cdd5c820cb6abeecaef38e2bb8db4860")
+
+    # Your Bot Token
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "7513275210:AAGgRzfVk1_nEaekouSY1kWmp0eqD3pNw-I")
+
+    # Your Admin User ID
     ADMIN_ID = int(os.environ.get("ADMIN_ID", "7944526906"))
 
+    # Your MongoDB Connection String
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "telegram_bot")
-    OWNER_DATABASE_CHANNEL = int(os.environ.get("OWNER_DATABASE_CHANNEL", -1002832802347))
     
-    # --- NEW: Using your VPS IP and Port for permanent links ---
-    PERMANENT_LINK_DOMAIN = os.environ.get("PERMANENT_LINK_DOMAIN", "http://65.21.183.36:5050")
-
-    # A default link for your shortener to use in the bot's PM.
-    SHORTENER_AD_LINK = os.environ.get("SHORTENER_AD_LINK", "https://google.com")
+    # --- NEW: Your VPS IP Address and Port ---
+    # The domain where your bot is running. Use your VPS IP.
+    # DO NOT include http://
+    VPS_IP = os.environ.get("VPS_IP", "65.21.183.36")
+    # The port your web server will run on.
+    VPS_PORT = int(os.environ.get("VPS_PORT", 5050))
+    
+    # The name of the file that stores your bot's username
+    BOT_USERNAME_FILE = "bot_username.txt"
