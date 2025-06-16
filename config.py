a1e04d73bb46589fcdd7cbaf3775bf173950332c
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-16 20:13:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -9,16 +9,6 @@ class Config:
     MONGO_URI = os.environ.get("MONGO_URI", "mongodb+srv://soniji:chaloji@cluster0.i5zy74f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "telegram_bot")
     
-    # --- Your main, private database channel ID ---
-    # The bot MUST be an admin in this channel.
-    OWNER_DATABASE_CHANNEL = int(os.environ.get("OWNER_DATABASE_CHANNEL", -1002832802347))
-    
-    # A default link for your shortener to use in the bot's PM.
-    SHORTENER_AD_LINK = os.environ.get("SHORTENER_AD_LINK", "https://google.com")
-
-    # The file that stores your bot's username for the web redirector
     BOT_USERNAME_FILE = "bot_username.txt"
-    
-    # Your VPS IP and Port for the web redirector
     VPS_IP = os.environ.get("VPS_IP", "65.21.183.36")
     VPS_PORT = int(os.environ.get("VPS_PORT", 7071))
